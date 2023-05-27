03426766e903a6effd13cb55b10ccd78e72b1305
Abhijit Ghogre <aghogre@whileone.in>
2023-05-27 19:17:43 +0530

    Deploy

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 
 export type DiceNumber = 1 | 2 | 3 | 4 | 5 | 6;
--- src/hooks/useAudio.ts
+++ src/hooks/useAudio.ts
@@ -1,5 +1,3 @@
-"use client";
-
 import { useEffect, useState } from "react";
 
 export const useAudio = (url: string) => {
